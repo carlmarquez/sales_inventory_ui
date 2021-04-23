b9b82f202e609036914bb1e25e79cb0b101ed75f
hannalalia <63851948+hannalalia@users.noreply.github.com>
2021-04-23 22:08:31 +0800

    added redirect to pos and login

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import {
 } from "react-router-dom";
 import MainUI from './components/mainUI/MainUI'
 import Pos from "./components/POS/Pos";
+import Login from "./components/Login/Logins";
 
 const App = () => {
     return (
         <Router>
             <switch>
-                {/*<Route exact path="/pos">*/}
-                {/*    <Pos/>*/}
-                {/*</Route>*/}
-                {/*<Route exact path="/login">*/}
-                {/*    <Login/>*/}
-                {/*</Route>*/}
+                <Route exact path="/pos">
+                    <Pos/>
+                </Route>
+                <Route exact path="/login">
+                    <Login/>
+                </Route>
                 <MainUI/>
             </switch>
         </Router>
