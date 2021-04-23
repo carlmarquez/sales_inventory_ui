fbf66b0224de69603e6947f174889fef82faeebe
hannalalia <63851948+hannalalia@users.noreply.github.com>
2021-04-24 00:04:27 +0800

    Update ProductCard.js

--- src/components/POS/ProductList/ProductCard.js
+++ src/components/POS/ProductList/ProductCard.js
@@ -6,7 +6,7 @@ const ProductCard = ({classes,price, name, id,picture}) => {
 
     return (
         <Paper className={classes.product} variant={'outlined'}>
-            <img src={`${serverEndpoint}product/getImage/${picture}`}/>
+            <img src={`${serverEndpoint}`+`product/getImage/${picture}`}/>
             <p style={{margin:0}}>{name}</p>
             <p style={{margin:0}}>{`${price} $`}</p>
             <TextField
