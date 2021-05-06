877d8241edce1d043dd3527605f438a042faea2d
hannalalia <63851948+hannalalia@users.noreply.github.com>
2021-05-06 15:20:19 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+import {
+    BrowserRouter as Router, Route,
+
+} from "react-router-dom";
+import Login from "./components/Login/Logins";
+import {Switch} from 'react-router';
+
+import {baseUrlNoAuth} from "./utils/axios/BaseUrl";
+import {tokenData} from "./utils/ServerEndPoint";
+import Skeleton from "@material-ui/lab/Skeleton";
+import {lazy, useEffect, useState, Fragment,Suspense} from 'react'
+
+const MainUi = lazy(() => import(`./components/mainUI/MainUI`));
+const Pos = lazy(() => import(`./components/POS/Pos`))
+const App = () => {
+
+
+    const [token, setToken] = useState(null)
+    const [user, setUser] = useState()
+    const [posOn, setPosOn] = useState(false)
+
+
+// localStorage.clear()
+
+    useEffect(() => {
+        const tokenLocal = localStorage.getItem('jars-token')
+        if (tokenLocal) {
+            setToken(tokenLocal)
+            const getUser = async () =>
+                await baseUrlNoAuth.post(tokenData, {token: tokenLocal}).then(e => {
+                    setUser(e.data)
+                })
+
+            getUser().then(ignored => {
+            })
+
+        }
+    }, [token])
+
+    useEffect(() => {
+
+        if (user && user.role === 1) {
+            setPosOn(true)
+        }
+
+    }, [user])
+
+    return (
+
+        <Router>
+            <Suspense fallback={<Skeleton variant={"rect"} animation="wave" style={{width: '100%', height: '70%'}}/>}>
+                {
+                    token === null ? <Login setUser={setUser}
+                                            setToken={setToken}/> :
+                        <Switch>
+
+                            {
+                                posOn === true ?
+                                    <Route path="/">
+                                        <Pos setPosOn={setPosOn} user={user}/>
+                                    </Route>
+
+                                    :
+                                    <Fragment>
+                                        <MainUi setPosOn={setPosOn} setUser={setUser} user={user}/>
+                                    </Fragment>
+                            }
+
+                        </Switch>
+
+                }
+            </Suspense>
+
+            {/*<Redirect to={"/"}/>*/}
+        </Router>
+
+    )
+
+}
+
+export default App;
